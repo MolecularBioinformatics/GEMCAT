beea3acdc653efcbd5fbb1d4711407d62ff3fd21
sauter-roland <sauter.roland@googlemail.com>
2023-07-31 22:43:00 +0200

    Test for seed integration

--- tests/test_seeds.py
+++ tests/test_seeds.py
@@ -1,18 +1,58 @@
 import pytest
 import pyreporter as pr
+from pathlib import Path
+from pandas import Series
+import numpy as np
 
-model_path = './tests/test_models/mini.xml'
+base_path = Path('./tests')
+model_path = base_path / 'test_models'
+seeds_model = model_path / 'seeds_test.csv'
+mini_path = model_path / 'mini.xml'
 expression_path = './tests/test_seq/expression_mini.csv'
+model_seeds = model_path / 'seeds_test.csv'
 
+eps = 10 ** -5
 
 def test_seed_load():
     seeds = [1., 2., 3., 4.]
-    model = pr.io.load_sbml_cobra(model_path)
+    model = pr.io.load_sbml_cobra(mini_path)
     model.load_metabolite_seeds(seeds)
     assert (model.seeds == seeds).all()
 
 def test_wrong_seed_load():
     seeds = [1., 2., 3., 4., 5.]
-    model = pr.io.load_sbml_cobra(model_path)
+    model = pr.io.load_sbml_cobra(mini_path)
     with pytest.raises(ValueError):
         model.load_metabolite_seeds(seeds)
+
+# found here: https://www.briggsby.com/personalized-pagerank
+def test_seed_integration():
+    expected = Series({
+        'A': 0.226558,
+        'B': 0.218752,
+        'C': 0.432226,
+        'D': 0.122464,
+    })
+    model = pr.io.load_csv(seeds_model)
+    seeds = [0., 0., 1., 0.]
+    gene_lvls = Series(dict(zip(
+        ['G1', 'G2', 'G3', 'G4', 'G5'], 5 * [1.]
+    )))
+    gpr = {
+        'R1': ['G1'],
+        'R2': ['G2'],
+        'R3': ['G2'],
+        'R4': ['G3'],
+        'R5': ['G3'],
+        'R6': ['G4'],
+        'R7': ['G5'],
+    }
+    ex = pr.Expression.ExpressionMapSingleAverage(gene_lvls, gpr)
+    model.load_expression(ex)
+    model.load_metabolite_seeds(seeds)
+    results = model.calculate()
+    assert isinstance(results, Series)
+    assert np.allclose(
+        results.values,
+        expected.values
+    )
